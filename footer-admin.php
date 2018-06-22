8bf3e27776fb83ec8ef915febd48d4c60e14d772
Raphael Koszalka <raphael@cochise.com.br>
2018-06-22 22:52:45 +0000

    Removed footer content

--- footer-admin.php
+++ footer-admin.php
@@ -1,11 +1,3 @@
-<div class="container">
-	<div class="col-md-12 text-center">
-		<hr>
-		<div class="pad35"></div>
-		<a href="https://www.raphael.website" style="color: #333" target="_blank">https://raphael.website</a>
-		<div class="pad35"></div>
-	</div>
-</div>
 <?php wp_footer(); ?>
 <script src="<?php echo get_template_directory_uri(); ?>/js/jquery.js"></script>
 <script src="<?php echo get_template_directory_uri(); ?>/js/bootstrap.min.js"></script>
