70cd748a403e4edc133e92ce3a10e7933de254b6
Raphael Koszalka <raphael@cochise.com.br>
2018-06-22 22:51:19 +0000

    Disabled panels

--- constants.php
+++ constants.php
@@ -2,10 +2,3 @@
 
 $base_url = 'http://' . $_SERVER[HTTP_HOST];
 $theme_name = 'Adminsitração Lavasul';
-
-$blog = array(
-  'title' => 'Blog',
-  'href' => ''
-);
-
-$post_categories = array($blog);
--- page-admin.php
+++ page-admin.php
@@ -6,124 +6,37 @@
 
 get_header('admin'); ?>
 
-<div>
-    <div class="col-md-3">
-        <div class="panel panel-primary">
-            <div class="panel-heading">
-                <h3 class="panel-title">Landing</h3>
-            </div>
-            <div class="panel-body">
-                <div class="row">
-                    <div class="col-md-6 text-center">
-                        <i class="fa fa-plus-circle fa-5x" aria-hidden="true"></i>
-                        <hr />
-                        <a href="https://raphael.website/api/admin/add/landing/">
-                            <button class="btn btn-block btn-default">
-                                Add Item
-                            </button>
-                        </a>
-                    </div>
-                    <div class="col-md-6 text-center">
-                        <i class="fa fa-list-alt fa-5x" aria-hidden="true"></i>
-                        <hr />
-                        <a href="https://raphael.website/api/admin/list/landing/">
-                            <button class="btn btn-block btn-default">
-                                List Items
-                            </button>
-                        </a>
-                    </div>
+<!-- these content will be looped -->
+<!-- <div class="col-md-3">
+    <div class="panel panel-primary">
+        <div class="panel-heading">
+            <h3 class="panel-title">Landing</h3>
+        </div>
+        <div class="panel-body">
+            <div class="row">
+                <div class="col-md-6 text-center">
+                    <i class="fa fa-plus-circle fa-5x" aria-hidden="true"></i>
+                    <hr />
+                    <a href="<?php // echo $base_url; ?>/admin/add/landing/">
+                        <button class="btn btn-block btn-default">
+                            Add Item
+                        </button>
+                    </a>
+                </div>
+                <div class="col-md-6 text-center">
+                    <i class="fa fa-list-alt fa-5x" aria-hidden="true"></i>
+                    <hr />
+                    <a href="<?php // echo $base_url; ?>/admin/list/landing/">
+                        <button class="btn btn-block btn-default">
+                            List Items
+                        </button>
+                    </a>
                 </div>
             </div>
         </div>
     </div>
-    <div class="col-md-3">
-        <div class="panel panel-primary">
-            <div class="panel-heading">
-                <h3 class="panel-title">Blog</h3>
-            </div>
-            <div class="panel-body">
-                <div class="row">
-                    <div class="col-md-6 text-center">
-                        <i class="fa fa-plus-circle fa-5x" aria-hidden="true"></i>
-                        <hr />
-                        <a href="https://raphael.website/api/admin/add/blog/">
-                            <button class="btn btn-block btn-default">
-                                Add Item
-                            </button>
-                        </a>
-                    </div>
-                    <div class="col-md-6 text-center">
-                        <i class="fa fa-list-alt fa-5x" aria-hidden="true"></i>
-                        <hr />
-                        <a href="https://raphael.website/api/admin/list/blog/">
-                            <button class="btn btn-block btn-default">
-                                List Items
-                            </button>
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="col-md-3">
-        <div class="panel panel-primary">
-            <div class="panel-heading">
-                <h3 class="panel-title">Resume</h3>
-            </div>
-            <div class="panel-body">
-                <div class="row">
-                    <div class="col-md-6 text-center">
-                        <i class="fa fa-plus-circle fa-5x" aria-hidden="true"></i>
-                        <hr />
-                        <a href="https://raphael.website/api/admin/add/resume/">
-                            <button class="btn btn-block btn-default">
-                                Add Item
-                            </button>
-                        </a>
-                    </div>
-                    <div class="col-md-6 text-center">
-                        <i class="fa fa-list-alt fa-5x" aria-hidden="true"></i>
-                        <hr />
-                        <a href="https://raphael.website/api/admin/list/resume/">
-                            <button class="btn btn-block btn-default">
-                                List Items
-                            </button>
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="col-md-3">
-        <div class="panel panel-primary">
-            <div class="panel-heading">
-                <h3 class="panel-title">Portfolio</h3>
-            </div>
-            <div class="panel-body">
-                <div class="row">
-                    <div class="col-md-6 text-center">
-                        <i class="fa fa-plus-circle fa-5x" aria-hidden="true"></i>
-                        <hr />
-                        <a href="https://raphael.website/api/admin/add/portfolio/">
-                            <button class="btn btn-block btn-default">
-                                Add Item
-                            </button>
-                        </a>
-                    </div>
-                    <div class="col-md-6 text-center">
-                        <i class="fa fa-list-alt fa-5x" aria-hidden="true"></i>
-                        <hr />
-                        <a href="https://raphael.website/api/admin/list/portfolio/">
-                            <button class="btn btn-block btn-default">
-                                List Items
-                            </button>
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+</div> -->
+
 <div class="clearfix"></div>
 
 <?php get_footer('admin'); ?>
