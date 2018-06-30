e1d1d2e1b535a113efd6e7c06fa55cb706a9baf8
Raphael Koszalka <rmkoszalka@gmail.com>
2018-06-30 19:52:05 -0300

    Need to loop through custom post types

--- page-admin.php
+++ page-admin.php
@@ -7,35 +7,35 @@
 get_header('admin'); ?>
 
 <!-- these content will be looped -->
-<!-- <div class="col-md-3">
+<div class="col-md-3">
     <div class="panel panel-primary">
         <div class="panel-heading">
-            <h3 class="panel-title">Landing</h3>
+            <h3 class="panel-title">Blog</h3>
         </div>
         <div class="panel-body">
             <div class="row">
                 <div class="col-md-6 text-center">
                     <i class="fa fa-plus-circle fa-5x" aria-hidden="true"></i>
                     <hr />
-                    <a href="<?php // echo $base_url; ?>/admin/add/landing/">
+                    <a href="<?php echo $base_url; ?>/admin/blog/adicionar/">
                         <button class="btn btn-block btn-default">
-                            Add Item
+                            Adicionar
                         </button>
                     </a>
                 </div>
                 <div class="col-md-6 text-center">
                     <i class="fa fa-list-alt fa-5x" aria-hidden="true"></i>
                     <hr />
-                    <a href="<?php // echo $base_url; ?>/admin/list/landing/">
+                    <a href="<?php echo $base_url; ?>/admin/blog/listar/">
                         <button class="btn btn-block btn-default">
-                            List Items
+                            Lisar
                         </button>
                     </a>
                 </div>
             </div>
         </div>
     </div>
-</div> -->
+</div>
 
 <div class="clearfix"></div>
 
