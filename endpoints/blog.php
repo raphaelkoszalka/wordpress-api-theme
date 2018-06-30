394fdd81367aafb1ff2fcde5cca862aee524d5de
Raphael Koszalka <rmkoszalka@gmail.com>
2018-06-30 20:26:15 -0300

    Update blog.php

--- endpoints/blog.php
+++ endpoints/blog.php
@@ -1,35 +1,35 @@
 <?php
+function blog( $data ) {
+  
+  $blog = get_posts(array(
+      'post_type'	=> array('blog'),
+      'posts_per_page'	=>	'1000'
+  ));
+  
+  $posts_arr = array();
+  
+  foreach ($blog as $p) {
+      $imagem = get_field('cover', $p->ID);
+      $gallery =  get_field('gallery', $p->ID);
+      $posts_arr[] = array(
+          'id' => $p->ID,
+          'title' => $p->post_title,
+          'sub_title' => $p->sub_title,
+          'slug' => $p->post_name,
+          'cover' => $imagem,
+          'date' => $p->date,
+          'intro' => $p->intro,
+          'full_content' => $p->full_content,
+          'gallery' => $gallery
+      );
+  }
 
-function blog( $data )
-{
-    $blog = get_posts(array(
-        'post_type'	=> array('blog'),
-        'posts_per_page'	=>	'1000'
-    ));
-
-    foreach ( $blog as $p )
-    {
-        $imagem = get_field('main_picture', $p->ID);
-        $gallery =  get_field('gallery', $p->ID);
-        $posts_arr[] = array(
-            'id' => $p->ID,
-            'title' => $p->post_title,
-            'sub_title' => $p->sub_title,
-            'slug' => $p->post_name,
-            'cover' => $imagem,
-            'date' => $p->date,
-            'intro' => $p->intro,
-            'full_content' => $p->full_content,
-            'gallery' => $gallery
-        );
-    }
-
-    return $blog;
+  $blog_return = $posts_arr;
+  return $blog_return;
 }
 
-add_action( 'rest_api_init', function ()
-{
-    register_rest_route( 'raphael/v1', '/blog',
+add_action( 'rest_api_init', function () {
+    register_rest_route( 'cochise/v1', '/blog',
         array(
             'methods' => 'GET',
             'callback' => 'blog',
